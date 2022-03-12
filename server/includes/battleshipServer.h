e568f10dc102b3b4f57518b3e3e7ccbe82c61b35
William Pradier <xantowill@gmail.com>
2022-03-12 19:00:31 +0100

    Initialized server process

---
+++ server/includes/battleshipServer.h
@@ -0,0 +1,15 @@
+#ifndef BATTLESHIPSERVER_H
+# define BATTLESHIPSERVER_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+
+typedef struct sockaddr_in s_sockaddr_in;
+
+
+#endif
